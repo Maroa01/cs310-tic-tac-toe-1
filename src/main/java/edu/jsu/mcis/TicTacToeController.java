fa97f92919773709979b3aefbf3500205e31b3e9
unknown <maroa.mara97@gmail.com>
2019-02-10 23:18:55 -0600

    PartII

--- src/main/java/edu/jsu/mcis/TicTacToeController.java
+++ src/main/java/edu/jsu/mcis/TicTacToeController.java
@@ -1,6 +1,8 @@
 package edu.jsu.mcis;
+import javax.swing.*;
+import java.awt.event.*;
 
-public class TicTacToeController {
+public class TicTacToeController implements ActionListener{
 
     private final TicTacToeModel model;
     private final TicTacToeView view;
@@ -12,9 +14,52 @@ public class TicTacToeController {
         /* Initialize model, view, and width */
 
         model = new TicTacToeModel(width);
-        view = new TicTacToeView();
+        view = new TicTacToeView(this, width);
         
     }
+	 public String getMarkAsString(int row, int col) {        
+        return (model.getMark(row, col).toString());        
+    }
+    
+    public TicTacToeView getView() {        
+        return view;        
+    }
+
+    @Override
+    public void actionPerformed(ActionEvent event) {
+        // INSERT YOUR CODE HERE
+		
+		String s = ((JButton) event.getSource()). getName();
+
+		int width = model.getWidth();
+		int row = Integer.parseInt(s.substring(6,7));
+		int col = Integer.parseInt(s.substring(7));
+		
+
+        if(model.makeMark(row, col)) {
+			view.updateSquares();
+			String result = model.getResult().toString();
+			switch(result) {
+				case "X":
+					view.showResult("X");
+					view.disableSquares();
+					break;
+				case "O":
+					view.showResult("O");
+					view.disableSquares();
+					break;
+				case "TIE":
+					view.showResult("TIE");
+					view.disableSquares();
+					break;
+				
+			}
+			
+		}
+    
+		
+    }
+
 
     public void start() {
     
@@ -26,7 +71,7 @@ public class TicTacToeController {
            using the View's "showInputError()" if the move is invalid. */
 
         // INSERT YOUR CODE HERE
-		while( !model.isGameover() ){
+		/*while( !model.isGameover() ){
 		
 			view.showBoard(model.toString());
             TicTacToeMove nextMove = view.getNextMove(model.isXTurn());
@@ -38,7 +83,7 @@ public class TicTacToeController {
 		
         /* After the game is over, show the final board and the winner */
 
-		view.showBoard(model.toString());
+		//view.showBoard(model.toString());
 
 		view.showResult(model.getResult().toString());
         
@@ -47,4 +92,5 @@ public class TicTacToeController {
     }
 	
 }
+
 
